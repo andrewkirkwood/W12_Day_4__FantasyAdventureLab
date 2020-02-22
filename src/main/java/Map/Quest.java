ff1d271f2b94fe739f95a10d4703e8a4b8da6f0b
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-02-22 14:47:31 +0000

    progress through quest, incorporate enemies as array list of party into a quest

--- src/main/java/Map/Quest.java
+++ src/main/java/Map/Quest.java
@@ -5,17 +5,20 @@ import Character.*;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.BitSet;
 
 public class Quest {
     private String name;
-    private ArrayList<Room> rooms;
     private Party party;
+    private ArrayList<Party> enemiesList;
+    private ArrayList<Room> rooms;
     private boolean isStillActive;
 
     public Quest(String name) {
         this.name = name;
-        this.rooms = new ArrayList<Room>();
         this.party = new Party(name);
+        this.enemiesList = new ArrayList<Party>();
+        this.rooms = new ArrayList<Room>();
         this.isStillActive = true;
     }
 
@@ -23,9 +26,11 @@ public class Quest {
         return this.party;
     }
 
-    public void addToParty(Player partyMember) {
-        this.party.getMembers().add(partyMember);
+    public ArrayList<Room> getRooms() {
+        return this.rooms;
     }
 
-
+    public ArrayList<Party> getEnemiesList() {
+        return this.enemiesList;
+    }
 }
--- src/test/java/QuestTest.java
+++ src/test/java/QuestTest.java
@@ -1,12 +1,40 @@
 import Map.Quest;
 import org.junit.Before;
+import org.junit.Test;
+import Character.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class QuestTest {
     private Quest quest;
+    private Wizard partyWizard;
 
     @Before
     public void before(){
         quest = new Quest("Ultima");
-
+        partyWizard = new Wizard("Him", 10, 5);
     }
+
+    @Test
+    public void starts_with_no_rooms(){
+        assertEquals(0, quest.getRooms().size());
+    }
+
+    @Test
+    public void starts_with_no_players(){
+        assertEquals(0, quest.getParty().getMembers().size());
+    }
+
+    @Test
+    public void can_add_member_to_party(){
+        quest.getParty().getMembers().add(partyWizard);
+        assertEquals(1, quest.getParty().getMembers().size());
+    }
+
+    @Test
+    public void enemies_starts_empty(){
+        assertEquals(0, quest.getEnemiesList().size());
+    }
+
+
 }
